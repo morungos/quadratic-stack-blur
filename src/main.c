f006571d47d02c797f8f83a3c96deed4a49f88bc
Stuart Watt <morungos@gmail.com>
2026-04-10 21:38:07 -0300

    basic core stack blur logic

--- src/main.c
+++ src/main.c
@@ -1,20 +1,45 @@
 #include <stdio.h>
 #include <stdint.h>
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
 #define TEST_SIZE 20
 
-uint8_t data[TEST_SIZE] = {
+int data[TEST_SIZE] = {
     0, 0, 0, 0, 0, 0, 0, 100, 0, 0, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0
 };
 
-static void blur(uint8_t *input, size_t count, size_t r) {
+static void blur(int *data, size_t count, size_t r) {
+    int buffer[20];
+    int qi = 0;
+    const int ds = 2 * r + 1;
+    int sum_in = 0;
+    int sum_out = 0;
+    int sum_stack = 0;
+    int stack_size = (r + 1) * (r + 1);
+
+    // Initialize the buffer
+    for(int i = 0; i < ds; i++) {
+        buffer[i] = 0;
+    }
+
     for(int i = 0; i < count; i++) {
-        input[i] = input[i];
+        data[i] = sum_stack / stack_size;
+        sum_stack -= sum_out;
+        sum_out -= buffer[qi];
+        int ni = MIN(i + r + 1, TEST_SIZE - 1);
+        buffer[qi] = data[ni];
+        sum_in += buffer[qi];
+        sum_stack += sum_in;
+        qi = (qi + 1) % ds;
+        int mi = (qi + r) % ds;
+        sum_out += buffer[mi];
+        sum_in -= buffer[mi];
     }
 }
 
 int main(int argc, char **argv) {
-    blur(data, TEST_SIZE, 4);
+    blur(data, TEST_SIZE, 2);
     fprintf(stdout, "{ ");
     for(int i = 0; i < TEST_SIZE; i++) {
         fprintf(stdout, "%d ", data[i]);
