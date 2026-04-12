e5b877c7083856ac9f2ebaa6aef324030633fa4b
Stuart Watt <morungos@gmail.com>
2026-04-11 22:50:49 -0300

    wip: removed old buffer addressing

--- working.js
+++ working.js
@@ -6,13 +6,13 @@ const BUFFER_SIZE = 2 * RADIUS + 1;
 const ACC_WIDTH = Math.floor((WIDTH + 1) / 2);
 const WEIGHT = ACC_WIDTH * (WIDTH - ACC_WIDTH) * WIDTH;
 
+// To start, assume we are zero to infinity
 const BUFFER = new Array(BUFFER_SIZE).fill(0);
 
 const DATA = [
     0, 0, 0, 0, 0, 10, 0, 0, 10, 20, 15, 11, 12, 5, 0, 0, 0, 0, 0, 0, 20, 10, 0, 0, 10, 0, 0, 0, 0, 0, 0
 ];
 
-let qi = 0;
 let left = 0, right = 0;
 let sum = 0;
 for(let i = 0; i < DATA.length; i++) {
@@ -33,8 +33,6 @@ for(let i = 0; i < DATA.length; i++) {
     
     sum -= left;
     right -= rem;
-
-    qi = (qi + 1) % BUFFER.length;
 }
 
 /**
