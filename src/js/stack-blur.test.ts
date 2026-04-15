d6b7809c78f86bd0de9a754ebf2a887f513695c7
Stuart Watt <stuart@morungos.com>
2026-04-15 17:01:33 -0300

    fix the test reading and writing

--- src/js/stack-blur.test.ts
+++ src/js/stack-blur.test.ts
@@ -1,7 +1,26 @@
 import { describe, it, expect } from 'vitest';
 
-describe('sum test', () => {
-    it('adds 1 + 2 to equal 3', () => {
-        expect(1 + 2).toBe(3);
+import fs from 'node:fs';
+import path from 'node:path';
+import { decode, encode } from "fast-png";
+
+describe('image test', () => {
+
+    it('basic stack blur', async () => {
+        
+        const test_file = path.join(__dirname, '..', '..', 'data', 'image.png')
+        const data = fs.readFileSync(test_file);
+        const png = decode(data);
+
+        // This is greyscale data, 1 byte per pixel.
+
+        png.data[0] = 12;
+        png.data[1] = 12;
+        png.data[3] = 12;
+
+        const buffer = encode(png);
+        const output_file = path.join(__dirname, '..', '..', 'output.png')
+        fs.writeFileSync(output_file, buffer);
     });
+
 });
