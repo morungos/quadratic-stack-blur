3411e11093a6a3f581b9c3b707c9c89e52ee4b1d
Stuart Watt <morungos@gmail.com>
2026-04-15 22:17:07 -0300

    change tables to reject factors which undershoot

--- src/js/integer-tables.ts
+++ src/js/integer-tables.ts
@@ -10,9 +10,15 @@ type Estimate = {
     epsilon: number;
 };
 
+// Note that shifting rounds down, which is annoying, because it means
+// that an exact value might well end up being lost. For example, if we
+// want to do division by 12 (a real example), we get a multiplier of
+// 341 and a shift of 12. Try that on 12 itself, and you get zero. not
+// one. So strictly, we should handle that.
+
 function tryFactors(estimate: Estimate, weight: number, multiplier: number, shift: number) {
-    const epsilon = Math.abs(weight - multiplier * 1.0/(1 << shift)) / weight;
-    if (multiplier <= MAX_MULTIPLIER && epsilon < estimate.epsilon) {
+    const epsilon = (multiplier * 1.0/(1 << shift) - weight) / weight;
+    if (epsilon >= 0 && multiplier <= MAX_MULTIPLIER && epsilon < estimate.epsilon) {
         estimate.multiplier = multiplier;
         estimate.shift = shift;
         estimate.epsilon = epsilon;
@@ -53,7 +59,7 @@ export function makeTables() {
         const acc_width = (r) >> 1;
         const weight = acc_width * (width - acc_width + 1) * (width + 1);
 
-        const estimate = getBestEstimate(1.0 / weight);
+        const estimate = getBestEstimate(1.0 / (weight));
         console.log("Value", r, weight, estimate);
     }
 }
